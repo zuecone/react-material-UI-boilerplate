8c1e3d0437c1c7fd0c60bfbc7099446203a709b1
Kenny Soetjipto <theokenny@hotmail.com>
2016-11-14 00:04:21 +1100

    Add App component and components folder

--- app/app.jsx
+++ app/app.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import TodoApp from 'TodoApp'
 
+var App = React.createClass({
+	render: function() {
+		//put your component below if needed
+	}
+})
+
 //render() - a function from ReactDOM package
 ReactDOM.render(
-			<h1>Boilerplate App</h1>,
+			<App />,
 			document.getElementById('app')
 );
