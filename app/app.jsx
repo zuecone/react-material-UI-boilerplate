19b22b5f391471e6418cd4f3de4fffbd3b5405ed
Kenny Soetjipto <theokenny@hotmail.com>
2016-11-17 22:48:09 +0000

    Removed duplicate createClass in app

--- app/app.jsx
+++ app/app.jsx
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom'
 
 var App = React.createClass({
 	render: function() {
-		return (
-			<div>
-				//Put your components tag here
-			</div>
-		)
-	}
-});
-
-//variable that is used inside render() function
-const appDOM = document.getElementById('app');
-
-var App = React.createClass({
-	render: function() {
 		//put your component below if needed
 	}
 })
